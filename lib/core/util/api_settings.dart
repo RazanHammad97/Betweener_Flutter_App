15591a246ebbf516cce5a481cf365c463f255f79
asmaaalastal <105522153+asmaaalastal@users.noreply.github.com>
2023-07-10 13:02:33 +0400

    update links file

--- lib/core/util/api_settings.dart
+++ lib/core/util/api_settings.dart
@@ -1,15 +1,13 @@
 class ApiSettings {
-  // var id = '';
   static const _baseUrl = "http://osamapro.online/api";
   static const register = '$_baseUrl/register';
   static const login = '$_baseUrl/login';
-  static const updateUserLocation = '$_baseUrl/update/8';
+  static const updateUserLocation = '$_baseUrl/update/';
   static const usersLink = '$_baseUrl/links'; //addLink with usersLink req
-  static const newUpdateFCM  = '$_baseUrl/fcm/{{id}}';
-  static const activeShare = '$_baseUrl/activeShare/{{id}}'; //delete with active share req
-  static const nearestSender = '$_baseUrl/activeShare/nearest/{{id}}';
-  static const linkReq = '$_baseUrl/links/5'; // delete and update link req
+  static const newUpdateFCM  = '$_baseUrl/fcm/';
+  static const activeShare = '$_baseUrl/activeShare/'; //delete with active share req
+  static const nearestSender = '$_baseUrl/activeShare/nearest/';
+  static const linkReq = '$_baseUrl/links/'; // delete and update link req
   static const followers = '$_baseUrl/follow'; // followers and addFollow link req
   static const search = '$_baseUrl/search';
-
 }
